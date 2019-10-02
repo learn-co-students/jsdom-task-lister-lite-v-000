accb302edf56ed1d30fb053e8fd2899f86a00511
Robin Doirin <robindoirin@gmail.com>
2019-10-02 14:24:43 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+function init() {
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', addTodo);
+}
+
+function addTodo(e) {
+  e.preventDefault();
+  const text = document.getElementById('new-task-description');
+  addTolist(text.value);
+  text.value = "";
+}
+
+function addTolist(text) {
+    const list = document.getElementById('tasks');
+    const item = document.createElement('li');
+    item.innerHTML = text;
+    list.appendChild(item);
+}
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  init();
 });
