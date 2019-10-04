7876f01167962a9492bf97a5f6a3b338dec6cde4
epic0448 <samuelogundiran@Yahoo.com>
2019-10-04 11:44:47 -0400

    refacotored

---
+++ src/taskList.js
@@ -0,0 +1,22 @@
+class TaskList {
+    constructor() {
+        this.tasks = []
+    }
+
+
+
+    createNewTask(description) {
+        const newTask = new TaskList(description);
+        this.tasks.push(newTask);
+    }
+
+
+    renderTasks() {
+        return this.tasks.map((task) => task.render()).join("")
+    }
+
+    deleteTask(description) {
+        this.tasks = this.tasks.filter((task) => task.description !== description);
+    }
+
+}
