f88a477a45ed97cdb7edb44cc8e4f664106b0636
ahotchkin <ahotchk1@yahoo.com>
2019-10-05 13:33:26 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let ul = document.getElementById("tasks");
+    let li = document.createElement("li");
+    let item = document.getElementById("new-task-description");
+    li.innerHTML = item.value += ` <button data-description=${item.value}>X</button>`
+    item.value = "";
+    ul.appendChild(li);
+    li.addEventListener("click", function(e) {
+      ul.removeChild(li);
+    })
+  });
 });
