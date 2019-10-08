5074a9e1917ebb79873f03c3cb94d88cce67844b
Heshie Brody <4hbrody@gmail.com>
2019-10-07 20:41:31 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('#create-task-form').addEventListener("submit", function(e) {
+    e.preventDefault();
+    addTaskToList();
+  })
 });
+
+function addTaskToList() {
+  let task = document.getElementById('new-task-description').value;
+  let taskList = document.getElementById('tasks');
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(task));
+
+  taskList.appendChild(li);
+}
+
+function removeTaskFromList(li) {
+
+}
