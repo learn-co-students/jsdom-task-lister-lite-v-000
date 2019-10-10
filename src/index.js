edba956b31cd358e43421bb3647905f2ed239240
mmelmer <mikemelmer@gmail.com>
2019-10-10 19:03:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.getElementById("create-task-form").addEventListener("submit", function(event) {
     event.preventDefault()
-    var 
     var li = document.createElement("li");
     var inputValue = document.getElementById("new-task-description")
     li.innerText = inputValue.value
