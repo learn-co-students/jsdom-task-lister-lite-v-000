d6c57516707f90d2badd2c2239eca888c8f10102
Rebecca Tong <rtong@buffalo.edu>
2019-10-14 22:44:24 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  // save the task from the input field
+  // click some sort of a submit button
+  // task can appear on the DOM after submit button has been activated
+
+  var elem = document.querySelector("#create-task-form")
+
+
+
+  function jake(event){
+    event.preventDefault();
+    var node = document.createElement("LI")
+    var textnode = document.createTextNode(document.getElementById("new-task-description").value)
+    node.appendChild(textnode)
+    document.getElementById("tasks").appendChild(node)
+  }
+
+  if (elem.addEventListener){
+    elem.addEventListener("submit",jake,false)
+  }
+
+
+
+
+
+
+
+})
