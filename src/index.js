fa77a9290489e66ab4890e34fb4d99c7d3e6d076
Leslie Stevens <flymaven@protonmail.com>
2019-10-19 16:37:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addItem()
+
 });
+
+function addItem() {
+  document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    let item = document.getElementById('new-task-description');
+    let li = document.createElement('li');
+    li.innerHTML = item.value;
+    document.getElementById('list').appendChild(li);
+    e.target.reset();
+  })
+}
