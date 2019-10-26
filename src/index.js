6d56d56bf784733c94d11dc9bcea6f7d604ed977
Nathaniel Shuster <nshu521992@gmail.com>
2019-10-26 00:16:45 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+});
+
+document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  let newTask = document.getElementById("new-task-description").value
+  console.log(newTask)
+  let list = document.getElementById("tasks")
+  let li = document.createElement("li");
+  li.innerHtml += newTask
+  list.appendChild(li)
 });
