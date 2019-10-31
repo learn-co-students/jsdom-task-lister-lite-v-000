cb10f36f4dce94295fba2db750c9eed1abdf5e83
github username <github email address>
2019-10-30 18:38:44 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+  const submitbutton = document.getElementById('main-content').querySelectorAll('form input');
+  submitbutton[1].addEventListener('click', function(event) {
+    let textsubmission = submitbutton[0].value;
+    let li = document.createElement('li');
+    li.innerHTML = textsubmission;
+    let ul = document.getElementById('tasks');
+    ul.appendChild(li)
+    event.preventDefault();
+  });
+
+  console.log("The DOM has loaded");
 });
