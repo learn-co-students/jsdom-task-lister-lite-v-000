2e612e401d09e48849419bdc2249858f76cac157
Meg Gutshall <meghan.gutshall@gmail.com>
2019-11-03 23:41:00 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let taskForm = document.getElementById("create-task-form");
+taskForm.addEventListener("submit", listToDo);
+
+function listToDo(item) {
+  item.preventDefault();
+  let task = document.getElementById("new-task-description");
+
+  let listItemText = document.createElement("p");
+  let listItem = document.createElement("li");
+  let taskList = document.getElementById("tasks");
+
+  listItemText.innerText = task.value;
+  listItem.appendChild(listItemText);
+  taskList.appendChild(listItem);
+
+  task.value = "";
+};
+
+
+// Get text
+
+// Assign task list to variable
+// Add list item to task list
+
+// Create paragraph element
+// Create list item
+// Add text to paragraph element
+// Add paragraph element to list item
