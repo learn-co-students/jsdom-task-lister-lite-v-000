0b4bf14ea36318187952daeafa87d4ad12d7286f
John Blank <veloblank@gmail.com>
2019-11-05 20:41:42 -0500

    Deliverable driven + delete stretch.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	const form = document.getElementById("create-task-form");
+	form.addEventListener("submit", function(e) {
+		e.preventDefault();
+		createTodo();
+	});
 });
+
+function createTodo() {
+	let todo = document.getElementById("new-task-description").value;
+	let todoLi = document.createElement("li");
+	todoLi.innerHTML =
+		todo +
+		"&nbsp;" +
+		"<button class='delete' onclick='deleteTask(this)'>x</button>";
+
+	let todoTaskList = document.getElementById("tasks");
+	todoTaskList.appendChild(todoLi);
+}
+
+function deleteTask(task) {
+	task.parentNode.remove();
+}
