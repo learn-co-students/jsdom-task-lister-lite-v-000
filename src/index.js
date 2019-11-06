af2b9baf79cba1a4164a8c34640ab01cdfde820b
Sushi Chanrai <sushi@pathworks.co>
2019-11-06 17:03:42 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  let ul = document.querySelector('ul#tasks');
+
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let taskDescrip = document.getElementById("new-task-description").value;
+      let userInput = document.getElementById("new-task-description")
+      ul.innerHTML += `<li>${taskDescrip} <button type ="button" onclick="remove(event)">Delete!</button></li>`;
+      userInput.value = "";
+    })
+
 });
+
+//delete fuction for onclick event 
+function remove(event) {
+  let button = event.target;
+  let li = button.parentElement;
+  li.remove();
+  return false;
+}
+
+
+
+//document.getElementById('my-textbox');
+// when to use querySelector and getElemetByIF
+// read up about value versus textContent
