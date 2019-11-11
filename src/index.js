9e30d115ff2ecb1dbc205771c26e5ed2242ec18c
Daniel Goldberg <danpaulgo@aol.com>
2019-11-10 19:12:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("submit-btn").addEventListener('click', function(event){
+	  event.preventDefault();
+	  let newItem = document.createElement("li");
+	  newItem.innerHTML = document.getElementById("new-task-description").value;
+	  let fullList = document.querySelector("ul");
+	  fullList.appendChild(newItem);
+	});
+// });
