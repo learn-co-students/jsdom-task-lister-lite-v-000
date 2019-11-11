43ab1cd20d3e113e7bb929e39f6aa72767d888f5
NaKia Whitby <whitby.nakia@gmail.com>
2019-11-11 19:26:28 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener("submit", function(e) {
+    value = document.getElementById('new-task-description').value
+    newItem = document.createElement('li');
+    newItem.textContent = `${value}`;
+    document.getElementById("tasks").appendChild(newItem);
+    event.preventDefault();
+    document.getElementById('new-task-description').value = ``;
+  })
 });
