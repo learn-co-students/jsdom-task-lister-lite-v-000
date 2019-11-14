390eb3ad6d565f459e086f667b0e31f25a552247
Shanise Barona <shanisebarona@gmail.com>
2019-11-13 18:39:45 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  //form
+  const form = document.getElementById('create-task-form')
+
+  // new task
+  const newTask = document.getElementById('new-task-description')
+  const li = document.createElement('li')
+
+  //list of todos
+  const todoList = document.getElementById('tasks')
+    
+  //listen to `submit` event on the form, use preventDefault()
+  form.addEventListener("submit", function(event) {
+    li.innerHTML = newTask.value
+    todoList.appendChild(li);
+
+    event.preventDefault();
+  });
+  
+
 });
+
