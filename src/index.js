a36ddf2d190a887d3e4d60e9bae850b731ccc317
corytveten <corytveten@gmail.com>
2019-11-18 19:32:38 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskDescription = document.getElementById('new-task-description');
+  document.querySelector('#submit').addEventListener("click", function(event) {
+    
+    event.preventDefault();
+  }, false);
 });
