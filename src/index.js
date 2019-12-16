a859efc5f8c820ff2a135f50cdd1983bd1fc11a4
dlyons8 <dlyons8@binghamton.edu>
2019-12-16 12:59:50 +0200

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  document.getElementById('create-task-form').addEventListener("submit", function(event) {
+    let list = document.getElementById('tasks');
+    let element = document.createElement('li');
+    let item = document.getElementById("new-task-description");
+    element.innerHTML = item.value;
+    list.appendChild(element);
+    item.value = ""
+    event.preventDefault();
+  }, false);
+
 });
