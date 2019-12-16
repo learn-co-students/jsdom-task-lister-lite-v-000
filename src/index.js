247bc4a0efad6dad31b279af07083d013e022c48
dlyons8 <dlyons8@binghamton.edu>
2019-12-16 22:30:35 +0200

    Done.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
   document.getElementById('create-task-form').addEventListener("submit", function(event) {
-    let list = document.getElementById('tasks');
+    let items = document.getElementById('tasks');
     let element = document.createElement('li');
     let item = document.getElementById("new-task-description");
-    element.innerHTML = item.value;
+    let tasks = document.querySelectorAll('div#list ul li button');
+
+    element.innerHTML = `${item.value} <button data-description="${item.value}">x</button>`;
     list.appendChild(element);
-    item.value = ""
+    item.value = "";
     event.preventDefault();
-  }, false);
+  });
+
+  tasks.addEventListener('click', function(event) {
+    const comment = event.target.parentElement
+    comment.remove();
+  });
 
 });
