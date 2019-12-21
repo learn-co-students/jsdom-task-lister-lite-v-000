a25aa576dd368632b3281411708735a351bc7bdd
Shaun McLoughlin <skmcloughlin@gmail.com>
2019-12-21 08:47:49 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const button = document.querySelectorAll("input[type=submit]")[0];
+
+function handleClick(e) {
+  let toDoItem = document.querySelectorAll("input[type=text]")[0].value;
+  if (toDoItem != "") {
+    let el = document.createElement("li");
+    el.innerHTML = toDoItem;
+    document.getElementById("tasks").appendChild(el);
+    document.querySelectorAll("input[type=text]")[0].value = "";
+  }
+}
+
+button.addEventListener("click", function(e) {
+  e.preventDefault();
+  handleClick(e);
 });
