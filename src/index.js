2a84bba1d00c160656850662f17df04f298f7d50
Scott Silverman <scottms724@gmail.com>
2019-12-21 17:08:11 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const taskSubmit = document.querySelectorAll("input[type=submit]")[0];
+
+function handleClick(e) {
+  let toDoTask = document.querySelectorAll("input[type=text]")[0].value;
+  if (toDoTask !== "") {
+    let el = document.createElement("li");
+    el.innerHTML = toDoTask;
+    document.getElementById("tasks").appendChild(el);
+    document.querySelectorAll("input[type=text]")[0].value = "";
+  }
+}
+
+taskSubmit.addEventListener("click", function(e) {
+  e.preventDefault();
+  handleClick(e);
 });
