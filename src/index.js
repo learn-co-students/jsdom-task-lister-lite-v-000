f4b4bd633c0a87ec803fd4734bdf38499b1781d9
Mohammed Noori <kmanoorib@gmail.com>
2020-02-24 03:06:25 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const newTask = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+  // const taskNode = document.createTextNode(taskList);
+  
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // alert("button was clicked");
+    
+    function addTask(task) {
+      // alert(task);
+      // this.taskNode.appendChild(`<li>${task}</li>`);
+      taskList.insertAdjacentHTML("afterend", `<li>${task}</li>`);
+    }
+    addTask(newTask.value);
+  });
 });
