a024fa78a51299e498f12be9ed158ded1af0d340
corytveten <corytveten@gmail.com>
2020-03-05 17:10:56 -0800

    event listener and appendTask

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const newTaskDescription = document.getElementById('new-task-description');
-  document.querySelector('#submit').addEventListener("click", function(event) {
-    
+  const newTaskForm = document.getElementById("create-task-form");
+
+  newTaskForm.addEventListener("submit", function(event){
     event.preventDefault();
-    
-  }, false);
+    const newTaskDescription = document.getElementById("new-task-description");
+    const newTask = document.createElement('li');
+    newTask.innerHTML = newTaskDescription.value;
+
+    appendTask(newTask);
+
+  });
+
+  function appendTask(task) {
+    document.getElementById('tasks').appendChild(task);
+  }
+  
 });
