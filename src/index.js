7464b0ec9eae207d59c01a46df876aceed9e2caa
dalia sawaya <dalia.sawaya@flatironschool.com>
2020-03-11 02:14:37 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  submit()
 });
+
+function submit() {
+  let form = document.getElementById("create-task-form")
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+    let input = document.getElementById("new-task-description")
+    let ul = document.getElementById("tasks")
+    let li = document.createElement("li")
+    li.innerHTML = input.value
+    ul.append(li)
+  })
+  
+}
