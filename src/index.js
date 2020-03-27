d91dcd9ca0a8cf2b3cfc9dd2f8cf16925a3018ac
inception88 <bljones36@gmail.com>
2020-03-26 22:13:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener("click", function(event) {
+    let input = document.getElementById("new-task-description").value;
+    document.getElementById("list").innerHTML += `${input}<br>`;
+    event.preventDefault();
+}, false);
 });
