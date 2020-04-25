1955d2031d2e79150b1188ffedf8b3c78f73ad91
Toni-Ty <antonique2479@yahoo.com>
2020-04-25 07:41:05 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code heredocument.addEventListener("DOMContentLoaded", () => {
+
+    let form = document.getElementById("create-task-form")
+    console.log("this is working",form)
+
+    let textBox =  document.getElementById("new-task-description")
+    // let button = form.querySelectorAll('input')[1]
+
+    let tasks = document.getElementById('tasks') // ul
+
+    form.addEventListener("submit", function(event) {
+      event.preventDefault()
+      console.log("button clicked")
+      let li = document.createElement("li")
+      li.innerText = textBox.value    //form.innerText
+      tasks.append(li)
+    })
+  });
 });
