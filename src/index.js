9426b2bbacfc049ec509ab905e8b6177ed960930
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-26 13:24:28 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", createTask);
+  
 });
+
+function createTask(event) {
+  event.preventDefault();
+  const ul = document.getElementById("tasks");
+  const input = document.getElementById("new-task-description");
+  let task = input.value;
+  const insertLi = document.createElement('li');
+  insertLi.innerText = task;
+  ul.appendChild(insertLi);
+};
