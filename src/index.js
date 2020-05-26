25298a2f3778677a063801ba3bcb9e79eeb1125e
github username <github email address>
2020-05-26 18:09:38 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    const task = document.getElementById("new-task-description")
+    const list = document.getElementById("list");
+  
+    document.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let todo = task.value;
+      list.innerHTML += `<div id="list-${todo}"><li>${todo}`;
+    });
+  });
