2ae78c564c721045ee47316620ed1378f6db45df
Eva Navarro <emanao@me.com>
2020-06-21 16:10:54 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit", (event) => {
+    getFormData();
+    event.preventDefault();
+  }, false);
 });
+
+function getFormData(){
+  let taskText = document.getElementById("new-task-description").value.trim();
+  if (checkInputText(taskText, "Task needs a name")) return;
+  addTask(taskText);
+}
+function checkInputText (taskText, msg){
+  if (taskText== ""){
+    alert(msg);
+    return true;
+  } else {
+    false;
+  }
+}
+function addTask(taskText) {
+  let li = document.createElement("li");
+  li.innerHTML = `${taskText} - ${addDelete()}`;
+  document.getElementById("tasks").appendChild(li);
+}
+function addDelete(){
+  return `<a href="#" class="removeTask">Delete</a>`
+}
