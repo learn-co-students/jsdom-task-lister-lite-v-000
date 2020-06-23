f95671dd4fd8acd181703d13843cab0005ff8578
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-23 22:19:04 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  var count = 0
+  
+  document.getElementById("create-task-form").addEventListener("submit", (event) => { 
+    event.preventDefault()
+    
+    count++
+    let value = event.target[0].value;
+    
+    addTask(count, value)
+  }, false);
 });
+
+function addTask(num, text){
+  let li = document.createElement("li")
+  li.innerHTML = text
+  li.setAttribute("id", `task_${num}`)
+  
+  li.addEventListener("click", (event) => {
+    event.preventDefault()
+    
+    let el = document.getElementById(event.target.id)
+    removeTask(el)
+  })
+  
+  document.getElementById("tasks").appendChild(li)
+}
+
+function removeTask(element){
+  element.parentNode.removeChild(element);
+}
