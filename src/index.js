e9d785c9954c6085b9397e7d6ae72ae1851a4b7c
tscotce <tatyana.celovsky@gmail.com>
2020-08-13 11:14:10 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+  }, false);
 });
+
