a681c1872c0168902d1040e0a293bebb26b114eb
Eric Johnson <ericjohnson0216@icloud.com>
2020-08-22 00:00:49 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("form#create-task-form").addEventListener("submit", function (event) {
+  document.querySelector("form#create-task-form").addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log("Is this working?")
+    console.log("Is this working?");
   }
 });
