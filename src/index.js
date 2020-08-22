4efa3536f87827ccb41393a3931325af8321e3a9
Eric Johnson <ericjohnson0216@icloud.com>
2020-08-22 00:25:27 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("form#create-task-form").addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log("Is this working?");
-  }
+    document.querySelector('#new-task-description')
+    // console.log("Is this working?");
+  })
 });
