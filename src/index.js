5bbd309e9de71b0ae9ae75190abc004e2b880b9d
Eric Johnson <ericjohnson0216@icloud.com>
2020-08-21 23:33:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", "click" function () => {
+document.addEventListener("click", function (event) {
+  document.getElementById("submit").event;
+  event.preventDefault();
   // your code here
 });
+// document.getElementById("submit").addEventListener("click", function (event) {
+//   document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+//   event.preventDefault();
+// }
