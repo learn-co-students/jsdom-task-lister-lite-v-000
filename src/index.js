7361cfa76974bfd1ed75004b1b4280fbfaec8845
osxgirl <osxgirl@mac.com>
2020-08-30 11:45:04 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", createTask);
 });
+
+function createTask(event) {
+  event.preventDefault();
+  const ulItem = document.getElementById("tasks");
+  const formInput = document.getElementById("new-task-description");
+  let newTask = formInput.value;
+  const insertElement = document.createElement('li');
+  insertElement.innerText = newTask;
+    
+  ulItem.appendChild(insertElement);
+    
+  
+};
+
