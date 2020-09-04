8818c20b90e9cd917be83c2d31fc9cd5223bab0c
Joshua Needham <joshuabneedham@gmail.com>
2020-09-04 09:30:27 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+	// your code here
+	const taskList = new TaskList();
+
+	const newTaskForm = document.getElementById('create-task-form');
+	const newTaskDescription = document.getElementById('new-task-description');
+	const newTaskPriority = document.getElementById('list');
+
+	const taskUl = document.getElementById('tasks');
+
+	const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+
+	console.log();
+
+	newTaskForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		taskList.createNewTask(newTaskDescription.value);
+		e.target.reset();
+		renderApp();
+	});
+
+	taskUl.addEventListener('click', (e) => {
+		if (e.target.nodeName === 'Button') {
+			taskList.deleteTask(e.target.dataset.description);
+			renderApp();
+		}
+	});
 });
