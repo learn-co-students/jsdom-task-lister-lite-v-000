90631fc1b9893167bd7692b461f4003470d312d6
Yazgylych Rahymov <y.rahymov@gmail.com>
2020-09-22 11:37:41 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("submit", (e) => {
+  e.preventDefault()
+  let newT = document.getElementById('new-task-description')
+  let li = document.createElement('li')
+
+  let tasks = document.getElementById('tasks')
+
+  li.appendChild(document.createTextNode(newT.value))
+  tasks.appendChild(li)
+  e.target.reset()
 });
