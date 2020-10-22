60afbb83307abee22707acedada83c2e2a0377c5
Laura Messa Travin <laura.travin@gmail.com>
2020-10-22 19:14:59 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+   function addTask(event){
+     const newLi = document.createElement('li');
+     newLi.textContent = form.querySelector('#new-task-description').value;
+
+    //  let textnode =  document.createTextNode(form.querySelector('#new-task-description').value);
+    //  newLi.appendChild(textnode);
+     document.getElementById('tasks').appendChild(newLi);
+     event.preventDefault();
+   }
+
+   const form =  document.getElementById('create-task-form');
+
+
+   form.addEventListener('submit',addTask);
+
 });
