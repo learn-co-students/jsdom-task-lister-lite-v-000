0ab58540b53fdcd1fae368c1ed3e8da193e193d1
Amil <github email address>
2020-11-16 09:14:50 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+// let userInputField = document.getElementById("new-task-description");
+const form =  document.getElementById('create-task-form');
+
+function createTask(event){
+    const createNewLi = document.createElement('li');
+    createNewLi.textContent = form.querySelector('#new-task-description').value;
+
+    document.getElementById('tasks').appendChild(createNewLi);
+    event.preventDefault();
+    event.target.reset();
+  }
+
+  form.addEventListener('submit',createTask);
+
 });
