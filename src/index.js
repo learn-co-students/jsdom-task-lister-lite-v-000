23260eb6406422d2b740edcfd4975c8d2f8bc942
PetyaDjanovska <petya.djanovska@gmail.com>
2021-04-13 13:32:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let content = document.getElementById('new-task-description');
+  const form = document.getElementById('create-task-form');
+  let list = document.getElementById('tasks');
+  let items = document.getElementsByClassName('item');
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+    addTask();
+  })
+
+  list.addEventListener("click", function(e) {
+    if(e.target && e.target.nodeName == 'li') {
+        removeTask(e);
+    }
+});
+
+
+  function addTask(){
+    let taskContent = document.createElement('li');
+      if (content.value == 0){
+        alert('enter valid task');
+        return
+      }
+    taskContent.appendChild(document.createTextNode(content.value));
+    taskContent.className = 'item';
+    list.appendChild(taskContent);
+  }
+
+  function removeTask(item){
+   item.target.remove();
+  }
 });
