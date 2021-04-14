f2d572da656c655442b1cc7c8a5fa41a1113b993
klovae <elyse.klova@gmail.com>
2021-04-13 22:42:31 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const submitInput = document.querySelector('form input[type=submit]');
+  
+  submitInput.addEventListener('click', function(event) {
+
+    const inputText = document.getElementById('new-task-description').value;
+    const toDoItem = document.createElement('li');
+
+    document.getElementById('tasks').appendChild(toDoItem);
+    toDoItem.innerHTML = inputText;
+    event.preventDefault();
+
+  });
+
 });
