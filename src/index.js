e3392cfbf425e0e9f5a6d2b6056f70a7a6c471c2
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-09 16:49:07 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  document.getElementById("create-task-form").addEventListener(
+    "submit",
+    function (event) {
+      event.preventDefault();
+
+      const taskName = document.getElementById("new-task-description").value;
+      const taskId = `${taskName.toLowerCase().split(" ").join("-")}`;
+      const priority = document.getElementById("priority").value;
+
+      const ul = document.getElementById("tasks");
+      const li = document.createElement("li");
+
+      li.innerHTML = `${taskName}<button id="btn-${taskId}">X</button>`;
+      li.id = taskId;
+
+      li.style.padding = "1rem";
+
+      if (priority == "high") {
+        li.style.backgroundColor = "red";
+      } else if (priority == "medium") {
+        li.style.backgroundColor = "yellow";
+      } else {
+        li.style.backgroundColor = "green";
+      }
+
+      ul.appendChild(li);
+    },
+    false
+  );
+
+  document.getElementById('tasks').addEventListener("click", function(event) {
+    const taskId = `${event.target.id.replace('btn-','')}`;
+    const taskItem = document.getElementById(taskId);
+    const taskList = taskItem.parentNode;
+
+    taskList.removeChild(taskItem);
+  })
 });
