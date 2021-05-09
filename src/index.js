65bf3432b5391946b86b52a1be906a41dcfcb959
github username <github email address>
2021-05-08 22:18:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded")
+});
+
+const form = document.getElementById("create-task-form");
+
+form.addEventListener("submit", function(result) {
+  result.preventDefault();
+  let submission = result.target[0].value;
+  console.log("You have a submission!");
+});
+
+function create_element_li() {
+  let li = document.createElement("li");
+  return li;
+};
+
+function append_element(value) {
+  const ul = document.getElementById("tasks");
+  let item = create_element_li()
+
+  item.innerHTML = value
+  ul.appendChild(item)
+};
+
+form.addEventListener("submit", function(result) {
+  result.preventDefault();
+  let submission = result.target[0].value;
+  console.log("You have a submission!");
+  append_element(submission)
 });
