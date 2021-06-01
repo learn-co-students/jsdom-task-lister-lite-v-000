d98897d5cf3101addaf2b2433ac180ac7e947fff
Thomas <Thomas.c.viles.jr@gmail.com>
2021-06-01 17:14:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const task = document.getElementById("new-task-description");
+  const list = document.getElementById("tasks");
+  document.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const taskLi = document.createElement("li");
+    taskLi.innerText = task.value
+    list.appendChild(taskLi);
+  });
 });
+
