0524822faae9b99ea3fe52ae36d301a4309a1f6d
Thomas <Thomas.c.viles.jr@gmail.com>
2021-06-01 17:38:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const task = document.getElementById("new-task-description");
-  const list = document.getElementById("tasks");
-  document.addEventListener("submit", function(event) {
-    event.preventDefault();
-    const taskLi = document.createElement("li");
-    taskLi.innerText = task.value
-    list.appendChild(taskLi);
-  });
+  document.addEventListener("submit", onSubmit);
 });
+
+let inc = 0
+
+const onSubmit = function(event) {
+    event.preventDefault();
+
+    const list = document.getElementById("tasks");
+    const task = document.getElementById("new-task-description");
+
+    const newTaskLi = document.createElement("li");
+    newTaskLi.id = "li"+inc
+    newTaskLi.innerText = task.value;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete"
+    deleteButton.id = "delete"+inc
+
+    list.appendChild(newTaskLi);
+    list.appendChild(deleteButton);
+};
+
+const onDelete = function(event) {
+  event.preventDefault();
+
+  const list = document.getElementById("tasks");
+  const task = document.getElementById("new-task-description");
+
+  const newTaskLi = document.createElement("li");
+  newTaskLi.innerText = task.value;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete"
+  deleteButton.id = "delete"
+
+  list.appendChild(newTaskLi);
+};
+
 
