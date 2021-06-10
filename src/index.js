5fdb0e0106d4cd3aa1eecddf3a64a00aac5b0f82
Ian Gutwinski <iangutwinski@gmail.com>
2021-06-10 11:36:10 -0400

    updated solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.querySelector('input[type="submit"]').addEventListener("submit", function(event) {
-    event.preventDefault();
-  }, false);
+  //document.querySelector('input[type="submit"]').addEventListener("submit", function(event) {
+  //  event.preventDefault();
+  //});
+
+  document.getElementById('create-task-form').addEventListener("submit", addToDo);
 
 });
+
+// when you click create new task, adds a new li element to ul where id = tasks with the content == whatever's in the field
+
+function addToDo(e) {
+  e.preventDefault()
+  let task = document.getElementById('new-task-description')
+  let formatted = document.createElement('li')
+  formatted.innerText = task.value
+  document.getElementById('tasks').appendChild(formatted)
+};
