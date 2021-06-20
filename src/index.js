a502fd780fe410f72887c1caa34e5aedc2aafbfd
halfields <hfields697@aol.com>
2021-06-19 19:19:42 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function()  {
   // your code here
+  addTask()
 });
+
+function addTask() {
+  const form  = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const task = document.getElementById("new-task-description").value;
+    const elList = document.getElementById("tasks");
+    const elItem = document.createElement("li");
+    elItem.innerText = task;
+    elList.appendChild(elItem);
+  } ; 
+};
+
